b8b3ce13923eb1010d9350106299df37f394c584
nichitaa <nichittaa@gmai.com>
2022-02-12 20:22:40 +0200

    add: handling now both streams (for `/tweets/1` and `tweets/2` endpoints)

--- lib/rtp_sse/command.ex
+++ lib/rtp_sse/command.ex
@@ -15,7 +15,16 @@ defmodule RTP_SSE.Command do
   def run({:twitter}, socket) do
     Logger.info("[Command] run :twitter - socket - #{inspect(socket)}")
 
-    {:ok, pid} =
+    # We need to handle 2 streams, so we need 2 LoggerRouters (aka worker pools)
+    # that will handle workers and tweets for each stream particularly
+
+    {:ok, router_1} =
+      DynamicSupervisor.start_child(
+        RTP_SSE.LoggerRouterDynamicSupervisor,
+        {RTP_SSE.LoggerRouter, socket: socket}
+      )
+
+    {:ok, router_2} =
       DynamicSupervisor.start_child(
         RTP_SSE.LoggerRouterDynamicSupervisor,
         {RTP_SSE.LoggerRouter, socket: socket}
@@ -24,7 +33,13 @@ defmodule RTP_SSE.Command do
     DynamicSupervisor.start_child(
       RTP_SSE.ReceiverWorkerDynamicSupervisor,
       {RTP_SSE.ReceiverWorker,
-       socket: socket, url: "http://localhost:4000/tweets/2", routerPID: pid}
+       socket: socket, url: "http://localhost:4000/tweets/1", routerPID: router_1}
+    )
+
+    DynamicSupervisor.start_child(
+      RTP_SSE.ReceiverWorkerDynamicSupervisor,
+      {RTP_SSE.ReceiverWorker,
+       socket: socket, url: "http://localhost:4000/tweets/2", routerPID: router_2}
     )
 
     {:ok, "[Command] run :twitter\r\n"}
